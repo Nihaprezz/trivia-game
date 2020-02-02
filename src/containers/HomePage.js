aca8006abdc647a27c84aab746e03268b81b3afe
nihaprezz <serg13prez@outlook.com>
2020-02-02 11:16:21 -0500

    updated home page text

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -9,7 +9,11 @@ class HomePage extends React.Component {
             <div className="home-page"> 
                 <div className="instructions">
                     <p>How to Play</p>
-                    <div>Using the Open Trivia Database test your knowledge! 10 questions will be shown depending on the category and difficulty picked. You can play by yourself or challenge a friend to see who knows trivia better! Thanks for playing!</div>
+                    <div>Using the Open Trivia Database test your knowledge! 10 questions will be shown depending on the category and difficulty picked. You can play by yourself or challenge a friend to see who knows trivia better!</div>
+                    <div>
+                        For two player games the 10 questions will be split. Players will take turns answering questions.
+                        Thanks for playing!
+                    </div>
                 </div>
 
                 <div className="player-count">
