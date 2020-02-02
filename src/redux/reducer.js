4f526a1085ecaf237a5f0356975f4ba0c2001288
nihaprezz <serg13prez@outlook.com>
2020-02-02 11:22:17 -0500

    started adding the scores in reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -18,9 +18,29 @@ const gameReducer = (state = [], action) => {
     }
 }
 
+const onePlayerReducer = (state = 0, action) => {
+    switch(action.type){
+        case "UPDATE_1P_SCORE":
+            return action.payload
+        default:
+            return state;
+    }
+}
+
+const twoPlayerReducer = (state = 0, action) => {
+    switch(action.type) {
+        case "UPDATE_2P_SCORE": 
+            return action.payload;
+        default: 
+            return state
+    }
+}
+
 const rootReducer = combineReducers({
     numOfPlayers: playerReducer,
-    gameData: gameReducer
+    gameData: gameReducer,
+    onePlayerScore: onePlayerReducer,
+    twoPlayerScore: twoPlayerReducer
 });
 
 export default rootReducer;
